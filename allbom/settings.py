97f6e38cd8df232c9d63b9001c4797cb0fe1f682
guibax <ganjabyte@openmailbox.org>
2018-07-04 22:23:36 -0300

    Changed the HTML for the site.

--- allbom/settings.py
+++ allbom/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'allbom.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '/home/guibax/Programs/django/django/blog/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
